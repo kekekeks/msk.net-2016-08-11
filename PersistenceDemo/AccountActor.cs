26d0f9c8f68312c5246e8530c047c187dc97de3b
Nikita Tsukanov <keks9n@gmail.com>
2016-08-11 11:03:15 +0300

    Added simple persistence demo

---
+++ PersistenceDemo/AccountActor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka;
+using Akka.Actor;
+using Akka.Persistence;
+
+namespace PersistenceDemo
+{
+    public class AccountActor : PersistentActor
+    {
+        public class WithdrawCommand
+        {
+            public WithdrawCommand(decimal amount)
+            {
+                Amount = amount;
+            }
+
+            public decimal Amount { get; }
+        }
+
+        public class DepositCommand
+        {
+            public DepositCommand(decimal amount)
+            {
+                Amount = amount;
+            }
+
+            public decimal Amount { get; }
+        }
+
+        public class GetBalanceCommand
+        {
+             
+        }
+
+        private decimal _balance;
+
+        void ProcessCommand(object cmd)
+        {
+            cmd.Match()
+                .With<DepositCommand>(d => _balance += d.Amount)
+                .With<WithdrawCommand>(d => _balance -= d.Amount);
+        }
+
+        protected override bool ReceiveRecover(object message)
+        {
+            ProcessCommand(message);
+            return true;
+        }
+
+        protected override bool ReceiveCommand(object message)
+        {
+            if (message is WithdrawCommand || message is DepositCommand)
+                Persist(message, ProcessCommand);
+            if (message is GetBalanceCommand)
+                Sender.Tell(_balance);
+            return true;
+        }
+
+        public override string PersistenceId => Self.Path.ToString();
+    }
+}
---
+++ PersistenceDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace PersistenceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sys = ActorSystem.Create("sys");
+
+            var aref = sys.ActorOf<AccountActor>("account1");
+            aref.Tell(new AccountActor.DepositCommand(100));
+
+            Console.WriteLine("Balance: " +
+                aref.Ask<decimal>(new AccountActor.GetBalanceCommand()).Result);
+
+
+            aref.Tell(PoisonPill.Instance);
+            try
+            {
+                Console.WriteLine("Balance: " +
+                    aref.Ask<decimal>(new AccountActor.GetBalanceCommand(), TimeSpan.FromSeconds(1)).Result);
+            }
+            catch
+            {
+                Console.WriteLine("Operation timed out, actor is dead");
+            }
+
+
+            aref = sys.ActorOf<AccountActor>("account1");
+            Console.WriteLine("Balance: " + aref.Ask<decimal>(new AccountActor.GetBalanceCommand()).Result);
+
+            Console.ReadLine();
+        }
+    }
+}
